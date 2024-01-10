4b127b2557997c937ecfdfb406051649f2eba462
LiuwuK <119620979+LiuwuK@users.noreply.github.com>
2024-01-10 19:22:28 -0300

    producer page

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def panelControl(request):
+    return render(request, 'dashboard_producer.html')
