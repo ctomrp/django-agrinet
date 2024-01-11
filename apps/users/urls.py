c51b43c59670fccf39a966f0065d4e9ea575b3f3
LiuwuK <119620979+LiuwuK@users.noreply.github.com>
2024-01-11 18:37:07 -0300

    merge

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path("", CustomLoginView.as_view(), name="login"),
     path("producer/", producer_dashboard, name="producer_dashboard"),
     path("client/", client_dashboard, name="client_dashboard"),
-    path('producer_dashboard/',views.panelControl,name='dashboard_producer'),
     path('my_products/',views.myProducts,name='my_products'),
 ]
