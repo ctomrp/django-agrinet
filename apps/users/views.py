8cbe6d2ec609e82a3b214bcefae78054d0e0550a
ctomrp <cristian.rico.palma@gmail.com>
2024-01-10 23:41:02 -0300

    login template tested

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import CustomAuthenticationForm
+
+
+class CustomLoginView(LoginView):
+    template_name = "login.html"
+    authentication_form = CustomAuthenticationForm
+
+    def form_valid(self, form):
+        user = form.get_user()
+
+        if hasattr(user, "userproducer"):
+            return redirect("producer_dashboard")
+        elif hasattr(user, "userclient"):
+            return redirect("client_dashboard")
+
+        return super().form_valid(form)
+
+
+def producer_dashboard(request):
+    return render(request, "producer_dashboard.html")
+
+
+def client_dashboard(request):
+    return render(request, "client_dashboard.html")
