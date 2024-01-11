845809ae84bdc3400d263f98d6b4c8e314143794
Raksaza <131925590+Raksaza@users.noreply.github.com>
2024-01-10 21:05:51 -0300

    formulario.html

--- apps/producer_application/models.py
+++ apps/producer_application/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class formulario(models.Model):
+    rut = models.CharField(max_length=12)
+    dv_rut = models.CharField(max_length=1)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+    correo = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    razon_social = models.CharField(max_length=255)
+    tipo_productor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
--- apps/producer_application/views.py
+++ apps/producer_application/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import formulario
+
 
 # Create your views here.
 def mostrar_formulario(request):
     return render(request, 'form.html')
+
+
+def productor(request):
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        rut = request.POST['rut']
+        dv_rut = request.POST['dv']
+        nombre = request.POST['primer-nombre']
+        apellido = request.POST['apellido-paterno']
+        edad = request.POST['edad']
+        correo = request.POST['correo']
+        telefono = request.POST['telefono']
+        razon_social = request.POST['razon-social']
+        tipo_productor = request.POST['tipo-productor']
+
+        # Crear instancia del modelo y guardar en la base de datos
+        formulario = formulario(
+            rut=rut,
+            dv_rut=dv_rut,
+            nombre=nombre,
+            apellido=apellido,
+            edad=edad,
+            correo=correo,
+            telefono=telefono,
+            razon_social=razon_social,
+            tipo_productor=tipo_productor
+        )
+        formulario.save()
+
+        return redirect('formulario')  # Puedes redirigir a donde desees después de guardar
+
+    return render(request, 'producer_application/form.html')
