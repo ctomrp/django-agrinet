7ebb7148e2d8153beb81e5b53be6c97d2f1b3e16
ctomrp <cristian.rico.palma@gmail.com>
2024-01-13 00:48:44 -0300

    sin login

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import UserClientForm, UserProducerForm
 from .models import UserClient, UserProducer
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
