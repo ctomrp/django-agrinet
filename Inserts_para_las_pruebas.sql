ff22b2d838b979c79139e39379fc24bb1a1a073b
ctomrp <cristian.rico.palma@gmail.com>
2024-01-14 17:59:57 -0300

    sprint 1

---
+++ Inserts_para_las_pruebas.sql
@@ -0,0 +1,21 @@
+/* Insert de Tipos de Productor */
+INSERT  INTO users_producertype VALUES (1, 'Productor de Frutas');
+INSERT  INTO users_producertype VALUES (2, 'Productor de Verduras');
+INSERT  INTO users_producertype VALUES (3, 'Productor de Tuberculos');
+INSERT  INTO users_producertype VALUES (4, 'Productor de Hortalizas');
+INSERT  INTO users_producertype VALUES (5, 'Productor de Legumbres');
+
+/* Insert de Nombres de Productos */
+INSERT INTO products_productname VALUES (1, 'Arándanos');
+INSERT INTO products_productname VALUES (2, 'Papas');
+INSERT INTO products_productname VALUES (3, 'Tomate');
+INSERT INTO products_productname VALUES (4, 'Limón');
+INSERT INTO products_productname VALUES (5, 'Sandía');
+INSERT INTO products_productname VALUES (6, 'Coco');
+
+/* Insert de Categorias de Productos */
+INSERT INTO products_productcategory  VALUES (1, 'Fruta');
+INSERT INTO products_productcategory  VALUES (2, 'Verdura');
+INSERT INTO products_productcategory  VALUES (3, 'Tuberculo');
+INSERT INTO products_productcategory  VALUES (4, 'Legumbre');
+INSERT INTO products_productcategory  VALUES (5, 'Hortaliza');
