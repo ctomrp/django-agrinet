470bde5c3db84cfc90da1d4000708cfee8729c61
OctaneMv33 <112354808+OctaneMv33@users.noreply.github.com>
2024-01-17 01:07:51 -0300

    Creación de ApplicationFormState (ahora si)

--- apps/producer_application/models.py
+++ apps/producer_application/models.py
@@ -3,6 +3,12 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
+class AplicationFormState(models.Model):
+    state_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.state_name
+
 class ApplicationForm(models.Model):
     dni = models.CharField(max_length=12, )
     first_name = models.CharField(max_length=100)
@@ -12,6 +18,7 @@ class ApplicationForm(models.Model):
     phone_number = models.CharField(max_length=20)
     bussiness_name = models.CharField(max_length=255)
     birth_date = models.DateField(verbose_name=("Fecha de nacimiento"))
+    state = models.ForeignKey(AplicationFormState, on_delete=models.CASCADE, verbose_name=("Status"))
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} {self.state}"
--- apps/producer_application/views.py
+++ apps/producer_application/views.py
@@ -2,29 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest
 
 from .forms import ApplicationFormForm
-from .models import ApplicationForm
+from .models import ApplicationForm, AplicationFormState
 
 
 def application_form_view(request):
     if request.method == 'POST':
         form = ApplicationFormForm(request.POST)
         if form.is_valid():
-            """
-            dni = form.cleaned_data['rut']
-            first_name = form.cleaned_data['nombre']
-            last_name = form.cleaned_data['apellido']
-            email = form.cleaned_data['correo']
-            address = form.cleaned_data['direccion']
-            phone_number = form.cleaned_data['telefono']
-            bussiness_name = form.cleaned_data['razon_social']
-            birth_date = form.cleaned_data['fecha_nac']
-            form_obj = ApplicationForm.objects.create(
-                dni = dni, first_name = first_name, last_name = last_name,
-                email = email, address = address, phone_number = phone_number,
-                bussiness_name = bussiness_name, birth_date = birth_date
-            )
-            """
-            form.save()
+            new_application = form.save(commit=False)
+            new_application.state = AplicationFormState.objects.get(id=10)
+            new_application.save()
             return redirect('login')
 
     return render(request, 'producer_application.html', {
